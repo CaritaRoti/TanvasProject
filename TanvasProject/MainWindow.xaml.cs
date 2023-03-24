3b5e1921018e0298e7b11f10f28b5dbe4c63a2a9
Carita Roti <carita.roti@tuni.fi>
2023-03-24 18:24:53 +0200

    initial commit

---
+++ TanvasProject/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tanvas.TanvasTouch.Resources;
+using Tanvas.TanvasTouch.WpfUtilities;
+
+namespace TanvasProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Tanvas.TanvasTouch.API.Initialize();
+            // ^ uncomment this and add Loaded="Window_Loaded" to the last row of the Window element in MainWindow.xaml
+            // currently throws error seemingly because the output needs to be read by the Tanvas tablet
+        }
+
+        TanvasTouchViewTracker viewTracker;
+
+        TView myView
+        {
+            get
+            {
+                return viewTracker.View;
+            }
+        }
+
+        void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewTracker = new TanvasTouchViewTracker(this);
+
+            var uri = new Uri("pack://application:,,/Assets/thingy.png");
+            var mySprite = PNGToTanvasTouch.CreateSpriteFromPNG(uri);
+
+            myView.AddSprite(mySprite);
+            Console.WriteLine("uwu");
+        }
+    }
+}
